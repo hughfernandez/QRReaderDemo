f361eda3d815fb4f6bc3c33cb6052dbe1496007f
Hugo G Fernandez R <hugh_fernandez@hotmail.com>
2018-09-13 00:34:04 -0500

    Add project files.

---
+++ QRReaderDemo/QRReaderDemo/QRReaderPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace QRReaderDemo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRReaderPage : ContentPage
+    {
+        public QRReaderPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        private async void Scanner()
+        {
+            var scannerPage = new ZXingScannerPage();
+
+            scannerPage.Title = "Lector de QR";
+            scannerPage.OnScanResult += (result) =>
+            {
+                scannerPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopAsync();
+                    DisplayAlert("Valor Obtenido", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushAsync(scannerPage);
+        }
+    }
+}
